ff549863ca1c3dc62484944afd6acc7fed800440
a290079770 <290079770@qq.com>
2018-04-18 20:57:34 +0800

    提交less配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Button } from 'antd';
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
