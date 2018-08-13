ebaad23b58090a1a11f3f9524d8b97fe303ffe16
罗文 <1083686250@qq.com>
2018-08-13 18:34:31 +0800

    新增文章item

--- miniProgram/pages/index/index.js
+++ miniProgram/pages/index/index.js
@@ -8,5 +8,30 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+    hotArticleList:[
+      {
+      	src:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      },
+      {
+      	src:'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      },
+      {
+      	src:'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      },
+    ],
+    newMoodList:[
+      {
+        title:'心情1心情1心情1心情1心情1心情1心情1心情1'
+      },
+      {
+        title:'心情1心情1心情1',
+      },
+      {
+        title:'心情1心情1心情1心情1心情1心情1心情1心情1心情1心情1心情1心情1心情1',
+      },
+    ],
   },
+  handleArticleClick(e) {
+    console.log(e.target.dataset);
+  }
 })
