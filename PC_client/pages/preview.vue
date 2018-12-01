1a4426cea99b073401078496cdcc45f984ef4a64
a290079770 <290079770@qq.com>
2018-12-01 18:33:55 +0800

    提交预览文章页面

--- PC_client/pages/preview.vue
+++ PC_client/pages/preview.vue
@@ -1,6 +1,43 @@
 <template>
-  <section class="mc preview">
-    preview
+  <section class="detail">
+    <section>&nbsp;</section>
+    <section class="mc detail-main">
+      <section class="detail-cont">
+        <h1 class="flex-center detail-title">
+          这是一片文章这是一片文章这是一片文章
+        </h1>
+
+        <p class="flex-center detail-info font gray9">
+          <span class="primary">@article</span>
+          <span class="font-lg info">Noven</span>
+          <span>2018-11-29</span>
+        </p>
+
+        <figure class="detail-cover">
+          <img src="~static/login_bg.jpg" class="detail-cover-img">
+        </figure>
+
+        <article class="font gray6 detail-content">
+          <p>这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章</p>
+          <p>这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章</p>
+          <p>这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章</p>
+          <p>这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章这是一片文章</p>
+        </article>
+
+      </section>
+    </section>
+    <section class="mc detail-author preview-btns-no-bg">
+      <div class="flex-center">
+        <el-button type="primary" class="preview-btn-submit">发布文章</el-button>
+        <el-button>返回编辑</el-button>
+      </div>
+    </section>
+
+<!--     <section class="mc detail-author ">
+      <section class="flex-center detail-author-cont">
+        点赞
+      </section>
+    </section> -->
   </section>
 </template>
 
@@ -20,4 +57,3 @@ export default {
   }
 }
 </script>
-
