cfd70ef267a4fa6af4b791480316a50ca6c592c3
a290079770 <290079770@qq.com>
2019-01-02 21:51:30 +0800

    提交H5个人中心页面

--- h5_client/pages/my.vue
+++ h5_client/pages/my.vue
@@ -1,23 +1,68 @@
 <template>
-  <section class="mc index">
-    my
-  </section>
+   <div class="my">
+     <div class="flex flex-justify-between my-published">
+       <div class="flex flex-justify-between my-published-item" @click="goTo" data-type="1">
+         <div class="flex flex-align-center flex-justify-center my-published-item-left">
+          <img src="~assets/icon/write-black.svg" class="my-published-item-img">
+         </div>
+         <div class="flex flex-justify-center flex-align-center my-published-item-right">
+          <span class="my-published-item-number">{{publishCount}} </span>   
+          <span>我的发布 </span>    
+         </div>
+       </div>
+       <div class="flex flex-justify-between my-published-item " @click="goTo" data-type="2">
+         <div class="flex flex-align-center flex-justify-center my-published-item-left">
+          <img src="~assets/icon/like.svg" class="my-published-item-img">
+         </div>
+         <div class="flex flex-justify-center flex-align-center my-published-item-right">
+          <span class="my-published-item-number">{{collectCount}} </span>   
+          <span>我的收藏 </span>    
+         </div>
+       </div>
+     </div>
+
+
+
+     <div class="flex flex-align-center my-oparate-item my-oparate-item-1" @click="updateCover">
+      <img src="~assets/icon/cover.svg" class="my-oparate-item-icon">
+      <span> 修改头像 </span>   
+     </div>
+
+     <div class="flex flex-align-center my-oparate-item" @click="goTo" data-type="3">
+      <img src="~assets/icon/nickname.svg" class="my-oparate-item-icon">
+      <span> 修改昵称 </span>   
+     </div>
+
+     <div class="flex flex-align-center my-oparate-item" @click="goTo" data-type="4">
+      <img src="~assets/icon/abstract.svg" class="my-oparate-item-icon">
+      <span> 修改简介 </span>   
+     </div>
+
+     <div class="flex flex-align-center my-oparate-item" @click="goTo" data-type="5">
+      <img src="~assets/icon/aboutus.svg" class="my-oparate-item-icon">
+      <span> 关于我们 </span>   
+     </div>
+
+     <div class="flex flex-align-center my-oparate-item">
+      <img src="~assets/icon/exit.svg" class="my-oparate-item-icon">
+      <span @click="logoutAction"> 退出登录 </span>   
+     </div>
+
+
+   </div>
 </template>
 
 <script>
 import { getArticleList } from '~/assets/service/articleService'
-import {Toast} from 'mint-ui';
 export default {
   data() {
     return {
-      
+      publishCount: 0,
+      collectCount: 0
     }
   },
 
   methods:{
-    a() {
-      Toast('hellow')
-    },
     /**
      * [getArticleList 获取文章列表]
      * @param  {[StriNg]} listName [对应this.data里的三个列表]
@@ -45,7 +90,11 @@ export default {
       let { order , ps } = orderBy.find(item => item.listName == listName);
       let { list } = await getArticleList(ps,1,'',order);
       this[listName] = list;
-    }
+    },
+
+    updateCover() {},
+    goTo() {},
+    logoutAction() {}
   },
   computed:{
     isLogin() {
